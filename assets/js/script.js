0b2bff35bd667fe46f64ff9bb9cb0c544ebc8c07
eliotCleveland <eliothead@live.com>
2022-06-20 20:47:37 -0400

    initial structure setup and quiz variable entered

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var quiz  = [
+    {question: "Located in Indonesia, _________ is the world’s most populous island, home to over 148 million people.",
+    choices: ["HTML", "Java", "JavaScript", "CSS", "Australia"],
+    answer: "Java"
+    },
+    {question: "The __________ is the standard juggling pattern and also a principle for styling properties of web pages.",
+    choices: ["Half-shower", "Shower", "Site Swap", "Cascade"],
+    answer: "Cascade"},
+    {question: "This US Vice-President infamously claimed they “took initiative in creating the internet” while on the campaign trail for presidential candidacy:",
+    choices: ["Kamala Harris", "Dan Quayle", "Spiro Agnew", "Al Gore"],
+    answer: "Al Gore"}
+];
