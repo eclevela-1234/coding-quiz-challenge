e821fc31be9698cd2b920103d6f85d9cc35ebb0e
eliotCleveland <eliothead@live.com>
2022-06-22 01:46:01 -0400

    finishing touches

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Coding-Quiz-Challenge
 A coding quiz to test your knowledge and showcase my skills! Score is calculated by the amount of time left on the clock - five seconds are deducted for each incorrect response. 
 
+![Screenshot of Application](./assets/images/Screenshot.png)
 ## Link
 https://eclevela-1234.github.io/coding-quiz-challenge/
 
@@ -9,7 +10,9 @@ https://eclevela-1234.github.io/coding-quiz-challenge/
 * DOM manipulation through raw JavaScript
 * Adding persistence through local storage 
 
-Note/Caveat: I didn't dynamically create all of the major elements of the application. There is still a basic structure in place in the HTML which served as a backbone of the styling. Some of the elements may have been repurposed in ways not as they were originally intended - but it works!!! Call it a novice attempt at DRY "Don't Repeat Yourself" through the CSS selectors.   
+Note/Caveat: I didn't dynamically create all of the major elements of the application. There is still a basic structure in place in the HTML which served as a backbone of the styling. Some of the elements may have been repurposed in ways not as they were originally intended - but it works!!! Call it a novice attempt at D.R.Y. "Don't Repeat Yourself" through the CSS selectors.   
 
 ## Final thoughts
-The instructor warned us that this assignment tends to be a heavy lift for students. It has a lot of parts and not starter code. While I did spend several hours completing it, I felt more confident than ever in my ability to plan and execute this assignment. As I've said before, I believed I had a leg up having programmed (mostly in Python) before, but I am realy happy with my progress so far. It is starting to click and that is a great feeling, especially since I am pushing to learn on my own as much as possible without the help of tutors.  
+The instructor warned us that this assignment tends to be a heavy lift for students. It has a lot of parts and no starter code. While I did spend several hours completing it, I felt more confident than ever in my ability to plan and execute this assignment. As I've said before, I believed I had a leg up having programmed (mostly in Python) before, but I am realy happy with my progress so far. It is starting to click and that is a great feeling, especially since I am pushing to learn on my own as much as possible without the help of tutors. 
+
+Enjoy!  
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,162 +1,196 @@
-
-var quiz  = [
-    {question: "Located in Indonesia, _________ is the world’s most populous island, home to over 148 million people.",
+// Quiz object
+var quiz = [
+  {
+    question:
+      "Located in Indonesia, _________ is the world’s most populous island, home to over 148 million people.",
     choices: ["HTML", "Java", "JavaScript", "CSS", "Australia"],
-    answer: "Java"
-    },
-    {question: "The __________ is the standard juggling pattern and also a principle for styling properties of web pages.",
+    answer: "Java",
+  },
+  {
+    question:
+      "The __________ is the standard juggling pattern and also a principle for styling properties of web pages.",
     choices: ["Half-shower", "Shower", "Site Swap", "Cascade"],
-    answer: "Cascade"},
-    {question: "This US Vice-President infamously claimed they “took initiative in creating the internet” while on the campaign trail for presidential candidacy:",
+    answer: "Cascade",
+  },
+  {
+    question:
+      "This US Vice-President infamously claimed they “took initiative in creating the internet” while on the campaign trail for presidential candidacy:",
     choices: ["Kamala Harris", "Dan Quayle", "Spiro Agnew", "Al Gore"],
-    answer: "Al Gore"}
+    answer: "Al Gore",
+  },
+  {
+    question: "True or False: jQuery is a men's fashion deligner.",
+    choices: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "The road to hell is paved with ____________",
+    choices: ["Legacy Code", "CSS Refactoring", "Good Intentions", "Boot Camp Challenges"],
+    answer: "Good Intentions",
+  },
+  {
+    question: "Sorting Key Value pairs numerically (such as a high score array) using JavaScript is  ____________",
+    choices: ["Easy", "Hard", "Impossible", "Convoluted"],
+    answer: "Convoluted",
+  },
+  {
+    question: "In the world of coding, Practice makes ____________",
+    choices: ["imperfect", "frustrated", "a good debugger/Googler", "All of the above"],
+    answer: "All of the above",
+  },
+  
 ];
-var time = 30;
-//Question Incrementer
-var questionIncrementer = 0 
+
+// Global Vars
+
+var time = 45;
+var questionIncrementer = 0;
 
 //element vars
-var timerEl = document.getElementById('timer');
-var highScoreEl = document.getElementById('high-scores');
-var questionEl = document.getElementById('question');
-var choicesEl = document.getElementById('choices');
-var answerEl = document.getElementById('answer');
-var dialogEl = document.getElementById('dialog');
+var timerEl = document.getElementById("timer");
+var highScoreEl = document.getElementById("high-scores");
+var questionEl = document.getElementById("question");
+var choicesEl = document.getElementById("choices");
+var answerEl = document.getElementById("answer");
+var dialogEl = document.getElementById("dialog");
 var quizBoxEl = document.getElementById("quiz-box");
-var inputEl = document.createElement('input');
+var inputEl = document.createElement("input");
+
+// Initialized Score Obj/arr
 var playerScore = {};
 var scoresArr = [];
 
-// timer
-var countdown = function () {
+// Page builds
 
-
-var timeInterval = setInterval(function() {
-    if (time >= 1) {
-        timerEl.textContent = "Time: " + time;
-        time--;
-    } else {
-        timerEl.textContent = "Time: " + time;
-        clearInterval(timeInterval);
-    }
-}, 1000)
-} 
-
-
-// display question
-var displayQuestions = function() {
-    
-    dialogEl.innerHTML = "";
-    choicesEl.innerHTML = "";
-    questionEl.textContent = quiz[questionIncrementer].question;
-    for (i=0; i < quiz[questionIncrementer].choices.length; i++) {
-        var listChoiceEl = document.createElement("li");
-        listChoiceEl.textContent = quiz[questionIncrementer].choices[i];
-        listChoiceEl.className = "btn"
-        choicesEl.appendChild(listChoiceEl);
-    }
-    questionIncrementer++;
-};
-var buttonHandler = function(event) {
-    var targetEl = event.target;
-    if (targetEl.textContent === "Log Score") {
-        
-        var playerScore = {"Player": inputEl.value, "Score":savedTime}
-        scoresArr.push(playerScore);
-        localStorage.setItem("scoresObj", JSON.stringify(scoresArr))
-        showScores();
-    }
-
-    else if (targetEl.textContent === "Begin!") {
-        countdown();
-        questionIncrementer = 0;
-        displayQuestions();
-    }
-    else if (targetEl.textContent !== quiz[questionIncrementer-1].answer) {
-        time = time - 5;
-        if (questionIncrementer < quiz.length) {
-            answerEl.textContent = "Wrong!";
-            displayQuestions();
-        } else {
-            savedTime = time; 
-            time = 0;
-            endQuiz();
-
-        }
-    }
-    else {
-        if (questionIncrementer < quiz.length) {
-            answerEl.textContent = "Correct!";
-            displayQuestions();
-        } else {
-            savedTime = time; 
-            time = 0;
-            endQuiz();
-        }
-    }
-}
 // landing screen and start
-var beginQuiz = function() {
-    questionEl.textContent = "Coding Quiz Challenge"
-    
-    dialogEl.innerHTML = "Try and beat the high score! Answer questions as accurately and quickly as possible. Time will be deducted for incorrect responses"; 
-    var startEl = document.createElement("button");
-    startEl.className = "btn";
-    startEl.textContent = "Begin!";
-    choicesEl.appendChild(startEl);
-    };
-var endQuiz = function(){
-    
-    answerEl.textContent = ""
-    dialogEl.innerHTML = "";
-    choicesEl.innerHTML = "";
-    questionEl.textContent = "All Done!"
-    dialogEl.innerHTML = "Your final score is " + savedTime + "! <br><br> Enter your name! <br><br>";
-    // create form element
-    
-    inputEl.setAttribute("type", "text");
-    inputEl.setAttribute("name", "name");
-    
-    dialogEl.appendChild(inputEl);
-    var logEl = document.createElement("button");
-    logEl.className = "btn";
-    logEl.textContent = "Log Score";
-    choicesEl.appendChild(logEl);
+var beginQuiz = function () {
+  questionEl.textContent = "Coding Quiz Challenge";
+  dialogEl.innerHTML =
+    "Try and beat the high score! Answer questions as accurately and quickly as possible. Time will be deducted for incorrect responses";
+  var startEl = document.createElement("button");
+  startEl.className = "btn";
+  startEl.textContent = "Begin!";
+  choicesEl.appendChild(startEl);
+};
 
- }
+// display questions
+var displayQuestions = function () {
+  initializeElements();
+  questionEl.textContent = quiz[questionIncrementer].question;
+  for (i = 0; i < quiz[questionIncrementer].choices.length; i++) {
+    var listChoiceEl = document.createElement("li");
+    listChoiceEl.textContent = quiz[questionIncrementer].choices[i];
+    listChoiceEl.className = "btn";
+    choicesEl.appendChild(listChoiceEl);
+  }
+  questionIncrementer++;
+};
 
-var loadScores = function(){
-    scoresObj = localStorage.getItem("scoresObj")
-    if(!scoresObj) {
-        console.log("there is no data");
-        return;
+// end of quiz, display score
+var endQuiz = function () {
+  initializeElements();
+  questionEl.textContent = "All Done!";
+  dialogEl.innerHTML =
+    "Your final score is " + savedTime + "! <br><br> Enter your name! <br><br>";
+  // create input element
+  inputEl.setAttribute("type", "text");
+  dialogEl.appendChild(inputEl);
+  var logEl = document.createElement("button");
+  logEl.className = "btn";
+  logEl.textContent = "Log Score";
+  choicesEl.appendChild(logEl);
+};
+
+//show scores at end and if event clicked
+var showScores = function () {
+  initializeElements();
+  questionEl.textContent = "High Scores";
+  var startEl = document.createElement("button");
+  startEl.className = "btn";
+  startEl.textContent = "Begin!";
+  choicesEl.appendChild(startEl);
+  if (!scoresArr[0]) {
+    return;
+  }
+
+  for (i = 0; i < scoresArr.length; i++) {
+    var ScoresEl = document.createElement("li");
+    ScoresEl.textContent = scoresArr[i].Player + " - " + scoresArr[i].Score;
+    dialogEl.appendChild(ScoresEl);
+  }
+};
+
+// background functions below
+
+// element initializer - D.R.Y.
+var initializeElements = function () {
+  dialogEl.innerHTML = "";
+  choicesEl.innerHTML = "";
+  questionEl.textContent = "";
+};
+
+//load scores
+var loadScores = function () {
+  scoresObj = localStorage.getItem("scoresObj");
+  if (!scoresObj) {
+    console.log("there is no data");
+    return;
+  }
+  scoresArr = JSON.parse(scoresObj);
+};
+
+// timer function
+var countdown = function () {
+  var timeInterval = setInterval(function () {
+    if (time >= 1) {
+      timerEl.textContent = "Time: " + time;
+      time--;
+    } else {
+      timerEl.textContent = "Time: " + time;
+      clearInterval(timeInterval);
     }
-    scoresArr = JSON.parse(scoresObj);
-    
+  }, 1000);
+};
+
+// Button Handler - lot going on here
+
+var buttonHandler = function (event) {
+  var targetEl = event.target;
+  if (targetEl.textContent === "Log Score") {
+    var playerScore = { Player: inputEl.value, Score: savedTime };
+    scoresArr.push(playerScore);
+    localStorage.setItem("scoresObj", JSON.stringify(scoresArr));
+    showScores();
+  } else if (targetEl.textContent === "Begin!") {
+    countdown();
+    questionIncrementer = 0;
+    displayQuestions();
+  } else if (targetEl.textContent !== quiz[questionIncrementer - 1].answer) {
+    time = time - 5;
+    if (questionIncrementer < quiz.length) {
+      answerEl.textContent = "Wrong!";
+      displayQuestions();
+    } else {
+      savedTime = time;
+      time = 0;
+      endQuiz();
     }
-    
-
-
-var showScores = function() {
-    answerEl.textContent = ""
-    dialogEl.innerHTML = "";
-    choicesEl.innerHTML = "";
-    questionEl.textContent = "High Scores"
-    var startEl = document.createElement("button");
-    startEl.className = "btn";
-    startEl.textContent = "Begin!";
-    choicesEl.appendChild(startEl);
-    if (!scoresArr[0]){
-        return;
+  } else {
+    if (questionIncrementer < quiz.length) {
+      answerEl.textContent = "Correct!";
+      displayQuestions();
+    } else {
+      savedTime = time;
+      time = 0;
+      endQuiz();
     }
+  }
+};
 
-    for (i =0; i< scoresArr.length; i++) {
-        var ScoresEl = document.createElement("li");
-        ScoresEl.textContent = scoresArr[i].Player + " - " + scoresArr[i].Score;
-        dialogEl.appendChild(ScoresEl);
-}
-}
+// event listeners
 choicesEl.addEventListener("click", buttonHandler);
 highScoreEl.addEventListener("click", showScores);
-loadScores()
+
+// function call to load page
+loadScores();
 beginQuiz();
